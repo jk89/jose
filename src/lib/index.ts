d3b967e7731abdeaf123f031b677de529f647b14
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-01-20 17:04:18 +0000

    working on exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,24 +1,24 @@
 
-import digest from '../runtime/digest';
-import parseKeyToJWK from '../runtime/key_to_jwk';
-import parseJWKToKey from '../runtime/jwk_to_key';
-import timingSafeEqual from '../runtime/timing_safe_equal';
-import checkCekLength from '../runtime/check_cek_length';
-import sign from '../runtime/sign';
-import verify from '../runtime/verify';
-import encrypt from '../runtime/encrypt';
-import decrypt from '../runtime/decrypt';
-import fetch from '../runtime/fetch';
-import random from '../runtime/random';
-import { encrypt as encryptPbes2kw, decrypt as decryptPbes2kw } from '../runtime/pbes2kw';
+import digest from '../runtime/digest.js';
+import parseKeyToJWK from '../runtime/key_to_jwk.js';
+import parseJWKToKey from '../runtime/jwk_to_key.js';
+import timingSafeEqual from '../runtime/timing_safe_equal.js';
+import checkCekLength from '../runtime/check_cek_length.js';
+import sign from '../runtime/sign.js';
+import verify from '../runtime/verify.js';
+import encrypt from '../runtime/encrypt.js';
+import decrypt from '../runtime/decrypt.js';
+import fetch from '../runtime/fetch.js';
+import random from '../runtime/random.js';
+import { encrypt as encryptPbes2kw, decrypt as decryptPbes2kw } from '../runtime/pbes2kw.js';
 
-export * from '../runtime/aesgcmkw';
-export * from '../runtime/generate';
-export * from '../runtime/ecdhes';
-export * from '../runtime/base64url';
-export * from '../runtime/webcrypto';
-export * from '../runtime/zlib';
-export * from '../runtime/interfaces';
+export * from '../runtime/aesgcmkw.js';
+export * from '../runtime/generate.js';
+export * from '../runtime/ecdhes.js';
+export * from '../runtime/base64url.js';
+export * from '../runtime/webcrypto.js';
+export * from '../runtime/zlib.js';
+// export * from '../runtime/interfaces.js';
 export { 
     parseKeyToJWK,
     parseJWKToKey,
