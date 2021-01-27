7971605dfb90f7336bdd40853a2393aab70a4c42
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2021-01-27 17:00:32 +0000

    exporting more like toEpoch, isObject

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,6 +11,18 @@ import decrypt from '../runtime/decrypt.js';
 import fetch from '../runtime/fetch.js';
 import random from '../runtime/random.js';
 import { encrypt as encryptPbes2kw, decrypt as decryptPbes2kw } from '../runtime/pbes2kw.js';
+import checkIvLength from './check_iv_length.js';
+import checkKeyType from './check_key_type.js';
+import checkP2s from './check_p2s.js';
+import decryptKeyManagement from './decrypt_key_management.js';
+import encryptKeyManagement from './encrypt_key_management.js';
+import isDisjoint from './is_disjoint.js';
+import isObject from './is_object.js';
+import toEpoch from './epoch.js';
+
+export * from './buffer_utils.js';
+export * from './cek.js';
+
 
 export * from '../runtime/aesgcmkw.js';
 export * from '../runtime/generate.js';
@@ -20,6 +32,14 @@ export * from '../runtime/webcrypto.js';
 export * from '../runtime/zlib.js';
 // export * from '../runtime/interfaces.js';
 export { 
+    toEpoch,
+    isObject,
+    isDisjoint,
+    decryptKeyManagement,
+    encryptKeyManagement,
+    checkP2s,
+    checkKeyType,
+    checkIvLength,
     parseKeyToJWK,
     parseJWKToKey,
     digest,
